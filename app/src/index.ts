c8e6fe83f798626f271e251d31b9727f2788f8b1
Henrik Herbst Sørensen <hhs@trifork.com>
2022-06-03 13:24:26 +0200

    Add express

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,8 +1,22 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+const server = app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
+
 process.on('SIGINT', handleTermination);
 process.on('SIGTERM', handleTermination);
 
 function handleTermination(args) {
     console.info(`Received ${args} shutting down`);
+    server.close(() => {
+        console.info('HTTP server closed');
+    });
 }
-
-console.log('Hello world');
