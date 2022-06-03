8976be94ebd16059c84e209162000bc4a3f592b8
Henrik Herbst Sørensen <hhs@trifork.com>
2022-06-03 12:58:35 +0200

    Add typescript, set up dockerfile

---
+++ app/src/index.ts
@@ -0,0 +1,8 @@
+process.on('SIGINT', handleTermination);
+process.on('SIGTERM', handleTermination);
+
+function handleTermination(args) {
+    console.info(`Received ${args} shutting down`);
+}
+
+console.log('Hello world');
